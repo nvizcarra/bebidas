34980c1ead848516086bb4b4347156a6614d14db
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-28 23:56:26 -0300

    Passing recipes to its component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Fragment } from 'react';
-import Header from './components/Header';
-import Formulario from './components/Formulario';
-import CategoriasProvider from './context/CategoriasContext';
-import RecetasProvider from './context/RecetasContext';
-
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListaRecetas from "./components/ListaRecetas";
+import CategoriasProvider from "./context/CategoriasContext";
+import RecetasProvider from "./context/RecetasContext";
 
 function App() {
   return (
     <CategoriasProvider>
       <RecetasProvider>
-        <Header/>
-        
+        <Header />
+
         <div className="container mt-5">
           <div className="row">
             <Formulario />
           </div>
+          <ListaRecetas />
         </div>
       </RecetasProvider>
     </CategoriasProvider>
