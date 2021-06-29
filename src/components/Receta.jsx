6c1808561e351422b29467b52c20934993531926
nvizcarra <nicolasvizcarra64@gmail.com>
2021-06-29 00:01:15 -0300

    Show recipe information

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -5,6 +5,17 @@ const Receta = ({receta}) => {
         <div className="col-md-4" mb-3>
             <div className="card">
                 <h2 className="card-header">{receta.strDrink}</h2>
+                {/* Mostrar información de las recetas */}
+                <img src="{receta.strDrinkThumb}" alt={`Ìmagen de ${receta.strDrink}`} className="card-img-top" />
+
+                <div className="card-body">
+                    <button
+                        type="button"
+                        className="btn btn-block btn-primary"
+                    >
+                        Ver receta
+                    </button>
+                </div>
             </div>
         </div>
     );
